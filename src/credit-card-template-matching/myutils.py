33cc670a2a1ef4adc9168ca5d9fa84c11bcbac62
weijunjiang123 <1366202829@qq.com>
2023-12-25 22:54:09 +0800

    credit card catch porject

---
+++ src/credit-card-template-matching/myutils.py
@@ -0,0 +1,17 @@
+import cv2
+
+#
+def sort_contours(cnts, method="left-to-right"):
+    reverse = False
+    i = 0
+
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+    return cnts, boundingBoxes
+
